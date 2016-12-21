ee90f2e7fc98065efbc658c297ee920b6f78f4bd
S V Aditya <aditya.siriguppa@gmail.com>
2016-12-22 00:39:30 +0530

    Amazon Correction

---
+++ myscript.js
@@ -0,0 +1,129 @@
+//alert("Click on the Shopping Spree icon beside the URL bar and select the website you want to get Cashback and fill the opened form. ");
+//window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+var url=window.location.href;
+if(url=="https://www.flipkart.com/" || url=="http://www.flipkart.com/" )
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://dl.flipkart.com/dl/?affid=adityasir","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://www.snapdeal.com/" || url=="http://www.snapdeal.com/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://www.snapdeal.com/?utm_source=aff_prog&utm_campaign=afts&offer_id=17&aff_id=70097","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://www.amazon.in/" || url=="http://www.amazon.in/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://aoebtech.blogspot.in/2016/12/cashback-for-amazon.html","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://www.freecharge.in/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=562&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://paytm.com/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=193&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://www.lenskart.com/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=32&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://www.oyorooms.com/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=546&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://www.makemytrip.com/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=677&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="http://www.jabong.com/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=62&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://www.tatacliq.com/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=1178&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="http://www.ticketgoose.com/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=1468&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://online.kfc.co.in/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=466&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://www.mcdelivery.co.in/")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=512&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
+else if(url=="https://pizzaonline.dominos.co.in/‎" || url=="http://www.dominos.co.in/‎")
+{
+  if (confirm("Want Cashback") == true) {
+      window.open("https://goo.gl/forms/8XWdOfRZFa","_self");
+      window.open("http://tracking.payoom.com/aff_c?offer_id=512&aff_id=17186","_blank");
+    } else {
+        alert("You are missing the valuable Cashback");
+    }
+}
